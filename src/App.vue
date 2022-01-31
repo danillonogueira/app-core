f561f76d7ff1698e8782835ad40063cb80bbdf26
Danillo Nogueira <dnll_nog@yahoo.com>
2022-01-31 18:07:54 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SideBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SideBar from './components/SideBar'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      SideBar
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .container {
+    width: 100%;
+    height: 100%;
+  }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script>
+  export default {
+    name: 'SideBar',
+  }
+</script>
+
+<style scoped lang="scss">
+  .sidebar {
+    width: 250px;
+    background: #EEE;
+    height: 100%;
+  }
+</style>
