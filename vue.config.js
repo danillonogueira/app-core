225d276c0bc4f9b22012c3f9f565b89c31e42329
Danillo Nogueira <dnll_nog@yahoo.com>
2022-02-09 19:36:38 -0300

    added bootstrap.js file for async importing and added publicPath to vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,25 @@
 module.exports = {
+  publicPath: 'http://localhost:8080/',
   chainWebpack: (config) => {
     config
       .plugin('module-federation-plugin')
-      .use(require('webpack').container.ModuleFederationPlugin, [{
-        name: 'app_core',
-        remotes: {
-          app_pkg_1: "app_pkg_1@http://localhost:8000/remoteEntry.js",
-        },
-        filename: "remoteEntry.js",
-        shared: {
-          vue: {
-            eager: true,
-            singleton: true,
+      .use(require('webpack').container.ModuleFederationPlugin, [
+        {
+          name: 'app_core',
+          remotes: {
+            app_pkg_1: 'app_pkg_1@http://localhost:8081/remoteEntry.js',
           },
-        },
-      }]);
+          filename: 'remoteEntry.js',
+          shared: {
+            vue: {
+              eager: true,
+              singleton: true,
+            },
+          },
+        }
+      ]);
   },
+  devServer: {
+    port: 8080
+  }
 };
